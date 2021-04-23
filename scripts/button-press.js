f01e38f96b8ce9d714277e4328b97c35268434d4
Caelen <wangc21@uw.edu>
2021-04-22 18:24:02 -0700

    terminal buttons

--- scripts/button-press.js
+++ scripts/button-press.js
@@ -1,12 +1,41 @@
 AFRAME.registerComponent('button-press', {
   init: function() {
+    const DURATION = 250
     let el = this.el
-    this.startRotation = function() {
-      el.emit('rotate');
+    let p = el.getAttribute('position');
+
+    this.moveIn = function() {
+      let params = {
+        property: 'position',
+        to: {
+          x: p.x, 
+          y: p.y, 
+          z: 7
+        },
+        dur: DURATION
+      }
+      el.setAttribute('animation', params);
+      setTimeout(() => {el.emit('depressed');}, DURATION)
     }
-    this.el.addEventListener('click', this.startRotation);
+
+    this.moveOut = function() {
+      let params = {
+        property: 'position',
+        to: {
+          x: p.x, 
+          y: p.y, 
+          z: 3
+        },
+        dur: DURATION
+      }
+      el.setAttribute('animation', params);
+    }
+
+    this.el.addEventListener('click', this.moveIn);
+    this.el.addEventListener('depressed', this.moveOut);
   },
   remove: function() {
-    this.el.removeEventListener('click', this.startRotation);
+    this.el.removeEventListener('click', this.moveIn);
+    this.el.removeEventListener('click', this.moveOut);
   }
 });
